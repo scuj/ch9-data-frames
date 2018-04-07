e3b0adc4e888ea756db0b0c8401d07df333c149d
scuj <scuj@uw.edu>
2018-04-06 23:08:04 -0700

    still need to figure out how to add columns

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,14 +2,14 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
+  win <- c(25, 26, 36, 48)
 
-
-# Create a vector of the number of points the Seahwaks have allowed to be scored
+  # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+  lose <- c(23, 6, 36, 17)
 
 # Combine your two vectors into a dataframe called `games`
-
+  Games <- (data.frame (win, lose))
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
